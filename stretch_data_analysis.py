46bee5654fab461a8cae5c5763ee974a208b9cc6
Michael Sindoni <mjs164@dhe.duke.edu>
2022-08-09 16:12:31 -0400

    cutoff cleaned version

--- stretch_data_analysis.py
+++ stretch_data_analysis.py
@@ -89,63 +89,34 @@ def p50_box_plot(df, conditions):
 
 def p50_current_cutoff_plot(df, cutoff_list):
     for i in range(len(cutoff_list)):
+        condition_list = conditions
+        conditions_dict = {}
+
+        #generates items (key/value) in the conditions dictionary. The key is the condition and the value is an empty list
+        for j in range(len(condition_list)):
+            conditions_dict[condition_list[j]] = [] 
+
+
         cutoff = cutoff_list[i]
+        df_cut = pd.DataFrame()
+        grouped = df.groupby(['condition', 'day', 'trial'])
+        for name, group in grouped:
+
+            # min_current = group['current']
+            # print(str(group['current']).min())
+            if group['current'].max() > cutoff:
+                df_cut = pd.concat([df_cut, group], ignore_index=True)
+
         #first making the raw p50 plots
-        grouped = df.groupby(['condition', 'trial'])
-        current_avg_list = []
-        mmHg_list = []
-        category_list = []
-        for name,group in grouped:
-            current_list = group['current'].tolist()
-            mmHg_list2 = group['mmHg'].tolist()
-            if max(current_list) > i:
-                for i in range(len(current_list)):
-                    current_avg_list.append(current_list[i])
-                    mmHg_list.append(mmHg_list2[i])
-                    category_list.append(name[0])
-            else:
-                continue
 
-        #adding 5mmHg to each sweep to correct for misreading pressure protocol
-        mmHg_list = [x + 5 for x in mmHg_list]
+        #creating palette for box plot color scheme. Will correct for number of conditions with 8 being the max (con-200Hz)
+        pal_colors = ['grey', '#464196', '#ffcfdc', 'maroon', 'goldenrod', 'navy', 'olivedrab', 'blueviolet']
+        pal_dict = {}
+        for i in range(len(condition_list)):
+            pal_dict[conditions[i]] = pal_colors[i]
 
-        df2 = pd.DataFrame({'category': category_list,
-                            'mmHg' : mmHg_list,
-                        'current_avg' : current_avg_list})
-
-        my_pal = ('grey', 'maroon', 'goldenrod', 'navy', 'olivedrab', 'blueviolet')
-        hue_order = ['con', '10', '20', '50', '100', '200']
-        sns.pointplot(data = df2, x = 'mmHg', y = 'current_avg', hue = 'category', hue_order = hue_order, palette = my_pal, ci = 'sd', estimator=np.mean,
-        scale = 0.7, errwidth = 1, capsize = 0.5, legend = False)
-        plt.show()
-
-        #second, making a normalized p50 curve
-        norm_i_avg_list = []
-        mmHg_list = []
-        category_list = []
-        for name,group in grouped:
-            current_list = group['norm_i'].tolist()
-            max_current_cutoff = group['current'].max()
-            mmHg_list2 = group['mmHg'].tolist()
-            if max_current_cutoff > cutoff:
-                for i in range(len(current_list)):
-                    norm_i_avg_list.append(current_list[i])
-                    mmHg_list.append(mmHg_list2[i])
-                    category_list.append(name[0])
-                else:
-                    continue
-                
-        #adding 5mmHg to each sweep to correct for misreading pressure protocol
-        mmHg_list = [x + 5 for x in mmHg_list]
-
-        df2 = pd.DataFrame({'category': category_list,
-                            'mmHg' : mmHg_list,
-                        'norm_i' : norm_i_avg_list})
-
-        my_pal = ('grey', 'maroon', 'goldenrod', 'navy', 'olivedrab', 'blueviolet')
-        hue_order = ['con', '10', '20', '50', '100', '200']
-        sns.pointplot(data = df2, x = 'mmHg', y = 'norm_i', hue = 'category', hue_order = hue_order, palette = my_pal, ci = 'sd', estimator=np.mean,
-        scale = 0.7, errwidth = 1, capsize = 0.5, legend = False)
+        sns.pointplot(data = df_cut, x = 'mmHg', y = 'norm_i', hue = 'condition', ci = 'sd', estimator=np.mean,
+        scale = 0.7, errwidth = 1, capsize = 0.5, legend = False, palette=pal_dict)
         plt.show()
 
 
@@ -186,6 +157,7 @@ def p50_current_cutoff_plot(df, cutoff_list):
         for name, group in n_grouped:
             len_category = len(group)
             print(name, ':', len_category)
+
 
 def current_time_correlation(df):
     grouped = df.groupby(['condition', 'day', 'trial'])
@@ -477,110 +449,45 @@ current_time_plot = current_time_correlation(df)
 t63 = t63_plot(df)
 ssc_peak = ssc_peak_ratio_plot(df)
 p50_time_plot = p50_time_correlation(df)
-cutoff_plots = p50_current_cutoff_plot(df, (20, 30, 40)) #partially cleaned
+cutoff_plots = p50_current_cutoff_plot(df, (20, 30, 40)) #cleaned
 resistances = pip_resistance(df) #cleaned
 #############################################################################################################
-cutoff_list = [20, 30, 40]
+print(df)
+
+sns.pointplot(x = 'mmHg',  y = 'norm_i', data = df, hue = 'condition')
+plt.show()
+
+cutoff_list = [20, 30, 40, 50, 60]
 
 for i in range(len(cutoff_list)):
     condition_list = conditions
     conditions_dict = {}
-    
+
     #generates items (key/value) in the conditions dictionary. The key is the condition and the value is an empty list
     for j in range(len(condition_list)):
         conditions_dict[condition_list[j]] = [] 
 
-    #go through each trial, get p50, and add to correct value list in dictionary
+
+    cutoff = cutoff_list[i]
+    df_cut = pd.DataFrame()
     grouped = df.groupby(['condition', 'day', 'trial'])
     for name, group in grouped:
-        p50_value = list(group['p50'])[0] #converts pandas core series to list and takes first element since they are all the same per group
-        cutoff = cutoff_list[i]
+
+        # min_current = group['current']
+        # print(str(group['current']).min())
         if group['current'].max() > cutoff:
-            ind_condition = name[0] #gets the condition
-            conditions_dict[ind_condition].append(p50_value) #appends list for that condition in the original dictionary
+            df_cut = pd.concat([df_cut, group], ignore_index=True)
 
-    #converts dictionary into a dataframe and creates box/strip plot
-    df2 = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in conditions_dict.items() ])).melt().dropna()
-
-    #adding 5 mmHg to correct for not starting at 0mmHg
-    df2['value'] = df2['value'] + 5
+    #first making the raw p50 plots
 
     #creating palette for box plot color scheme. Will correct for number of conditions with 8 being the max (con-200Hz)
     pal_colors = ['grey', '#464196', '#ffcfdc', 'maroon', 'goldenrod', 'navy', 'olivedrab', 'blueviolet']
     pal_dict = {}
     for i in range(len(condition_list)):
         pal_dict[conditions[i]] = pal_colors[i]
-    sns.boxplot(data = df2, x = 'variable', y = 'value', palette=pal_dict)
-    sns.stripplot(data = df2, x = 'variable', y = 'value', color = 'black')
-    plt.ylim(0, 40)
-    plt.show()
 
-    #getting n for each category
-    n_grouped = df2.groupby(['variable'])
-    for name, group in n_grouped:
-        len_category = len(group)
-        print(name, ':', len_category)
-
-
-###################################################################
-for i in range(len(cutoff_list)):
-    cutoff = cutoff_list[i]
-    #first making the raw p50 plots
-    grouped = df.groupby(['condition', 'trial'])
-    current_avg_list = []
-    mmHg_list = []
-    category_list = []
-    for name,group in grouped:
-        current_list = group['current'].tolist()
-        mmHg_list2 = group['mmHg'].tolist()
-        if max(current_list) > i:
-            for i in range(len(current_list)):
-                current_avg_list.append(current_list[i])
-                mmHg_list.append(mmHg_list2[i])
-                category_list.append(name[0])
-        else:
-            continue
-
-    #adding 5mmHg to each sweep to correct for misreading pressure protocol
-    mmHg_list = [x + 5 for x in mmHg_list]
-
-    df2 = pd.DataFrame({'category': category_list,
-                        'mmHg' : mmHg_list,
-                    'current_avg' : current_avg_list})
-
-    my_pal = ('grey', 'maroon', 'goldenrod', 'navy', 'olivedrab', 'blueviolet')
-    hue_order = ['con', '10', '20', '50', '100', '200']
-    sns.pointplot(data = df2, x = 'mmHg', y = 'current_avg', hue = 'category', hue_order = hue_order, palette = my_pal, ci = 'sd', estimator=np.mean,
-    scale = 0.7, errwidth = 1, capsize = 0.5, legend = False)
-    plt.show()
-
-    #second, making a normalized p50 curve
-    norm_i_avg_list = []
-    mmHg_list = []
-    category_list = []
-    for name,group in grouped:
-        current_list = group['norm_i'].tolist()
-        max_current_cutoff = group['current'].max()
-        mmHg_list2 = group['mmHg'].tolist()
-        if max_current_cutoff > cutoff:
-            for i in range(len(current_list)):
-                norm_i_avg_list.append(current_list[i])
-                mmHg_list.append(mmHg_list2[i])
-                category_list.append(name[0])
-            else:
-                continue
-            
-    #adding 5mmHg to each sweep to correct for misreading pressure protocol
-    mmHg_list = [x + 5 for x in mmHg_list]
-
-    df2 = pd.DataFrame({'category': category_list,
-                        'mmHg' : mmHg_list,
-                    'norm_i' : norm_i_avg_list})
-
-    my_pal = ('grey', 'maroon', 'goldenrod', 'navy', 'olivedrab', 'blueviolet')
-    hue_order = ['con', '10', '20', '50', '100', '200']
-    sns.pointplot(data = df2, x = 'mmHg', y = 'norm_i', hue = 'category', hue_order = hue_order, palette = my_pal, ci = 'sd', estimator=np.mean,
-    scale = 0.7, errwidth = 1, capsize = 0.5, legend = False)
+    sns.pointplot(data = df_cut, x = 'mmHg', y = 'norm_i', hue = 'condition', ci = 'sd', estimator=np.mean,
+    scale = 0.7, errwidth = 1, capsize = 0.5, legend = False, palette=pal_dict)
     plt.show()
 
 
@@ -621,3 +528,5 @@ for i in range(len(cutoff_list)):
     for name, group in n_grouped:
         len_category = len(group)
         print(name, ':', len_category)
+
+
